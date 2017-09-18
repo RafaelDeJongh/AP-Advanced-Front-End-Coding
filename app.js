416ebc4ee1897ff65dccb8c298eeeff4bee92a38
Rafaël De Jongh <rafael_boba_fett@msn.com>
2017-09-19 01:49:15 +0200

    Updated view and posts.

--- app.js
+++ app.js
@@ -14,15 +14,13 @@ app.use(function requestHandler(req,res,next){
 app.set('postsFile', require('./config/posts.json'));
 app.set('categoryFile', require('./config/category.json'));
 
-app.set("views", path.resolve(__dirname, "views"));
+app.set("views",path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set('port', (process.env.PORT || 5000));
+app.set('port',(process.env.PORT || 5000));
 app.use(express.static('public'))
 
 app.use(require("./routes/root_router"));
 app.use(require("./routes/posts_router"));
 app.use(require("./routes/category_router"));
 
-app.listen(app.get('port'), function() {
-	console.log('Node luistert op poort', app.get('port'));
-});
+app.listen(app.get('port'),function(){console.log('Node server is running on port:',app.get('port'));});
