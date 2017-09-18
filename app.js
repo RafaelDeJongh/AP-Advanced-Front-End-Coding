0009d2ad35fdd7b7055d49358160a597d42dc504
Rafaël De Jongh <rafael_boba_fett@msn.com>
2017-09-18 19:45:16 +0200

    updated categories

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require("path");
 var app = express();
 
 app.set('postsFile', require('./config/posts.json'));
-app.set('categorieenFile', require('./config/categorieen.json'));
+app.set('categoryFile', require('./config/category.json'));
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -12,7 +12,7 @@ app.use(express.static('public'))
 
 app.use(require("./routes/root_router"));
 app.use(require("./routes/posts_router"));
-app.use(require("./routes/categorieen_router"));
+app.use(require("./routes/category_router"));
 
 app.listen(app.get('port'), function() {
 	console.log('Node luistert op poort', app.get('port'));
--- routes/root_router.js
+++ routes/root_router.js
@@ -3,7 +3,7 @@ var root = express.Router();
 root.get('/', function(req,res){
 	res.render("index",{
 		posts:req.app.get('postsFile').posts,
-		categorieen:req.app.get('categorieenFile').categorieen
+		category:req.app.get('categoryFile').category
 	});
 });
 module.exports = root;
