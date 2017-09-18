b4f647fc1994720253fbc391f4f9498c8579e238
Rafaël De Jongh <rafael_boba_fett@msn.com>
2017-09-18 21:13:07 +0200

    Added Security Headers

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
+function requestHandler(req,res){
+	res.setHeader("Developed-By","Rafael De Jongh");
+	res.setHeader("Content-Security-Policy","default-src https:; img-src https: data: blob:; font-src https: data:; script-src https:; style-src https:;");
+	res.setHeader("X-Frame-Options","SAMEORIGIN");
+	res.setHeader("X-XSS-Protection","1; mode=block");
+	res.setHeader("Referrer-Policy","no-referrer-when-downgrade");
+}
+
 app.set('postsFile', require('./config/posts.json'));
 app.set('categoryFile', require('./config/category.json'));
 
